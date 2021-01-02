a72af0bb1ac1510c9ccc0cb6e6d0c87c1842500b
Your Name <you@example.com>
2021-01-02 20:33:35 +0800

    main printf

---
+++ test1.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main()
+{
+	int a = 10, b = 20;
+	if (a > b) printf("%d\n", a);
+	else printf("%d\n", b);
+	return 0;
+
+}
+//int main()
+//{
+//	int a = 10;
+//	int b = 10;
+//	int sum = 0;
+//	sum = a + b;
+//	printf("%d\n", sum);
+//	return 0;
+//}
+//int main()
+//{
+//	printf("Hello World!");
+//		return 0;
+//}
