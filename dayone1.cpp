1c17668b9627ab4545205557e4e6cde5fa01b7cd
Your Name <you@example.com>
2021-01-02 21:16:57 +0800

    常量的定义

---
+++ dayone1.cpp
@@ -0,0 +1,63 @@
+#include<stdio.h>
+//int main()
+//{
+//	enum enumtest {test1,test2};
+//	char *s[] = { "test1","test2" };
+//	printf("%d\n", test2);
+//	printf("%s\n", s[test2]);
+//	return 0;
+//}
+
+#define MAX 100//定义的标识符常量
+//enum SEX { //枚举常量
+//	Male,
+//	Female,
+//	Secret
+//};
+//int main()
+//{
+//	char *s[] = { "Male","Female" };
+//	printf("%d\n",Male);
+//	printf("%s\n",s[test2]);
+//	return 0;
+//}
+
+//int main()
+//{
+////常量的分类 
+//	enum SEX z = Male;//枚举常量
+//
+//	3.14;//字面常量
+//	printf("%d\n", MAX);
+//	const int a=10;
+//	int A[a] = {0};//从0开始标号 所有元素全等于0；
+//	int b = 10;
+//	//int B[b] = { 0 };b为变量不能用于定义数组；
+//	int i = 0;
+//	for(i=0;i<10;i++)
+//	printf("A[i]=%d\n", A[i]);
+//	return 0;
+//}
+////局部变量和全局变量
+//int global = 2019;//全局变量后面须带分号
+//int main()
+//{
+//	int local = 2018;//局部变量
+//	int global = 2020;
+//		printf("global=%d", global);
+//	return 0 ;
+//}
+//int main()
+//{
+//	//统计各个变量的字节数
+//	printf("char= %d\n",sizeof(char));
+//	printf("int= %d\n", sizeof(int));
+//	printf("short= %d\n", sizeof(short));
+//	printf("long= %d\n", sizeof(long));
+//	printf("long long= %d\n", sizeof(long long));
+//	printf("float= %d\n", sizeof(float));
+//	printf("double= %d\n", sizeof(double));
+//	printf("long double= %d\n", sizeof(long double));
+//	printf("unsigned= %d\n", sizeof(unsigned));
+//	return 0;
+//}
