5e796b8ef7cffcc1a99f8cc43931276e36ce898c
18792720221 <2285633822@qq.com>
2021-01-07 14:23:38 +0800

    add

---
+++ add.cpp
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<windows.h>
+
+
+int Add(int x, int y) //º¯Êý¶¨Òå
+{
+	int z;
+	z = x + y;
+	return z;
+}
