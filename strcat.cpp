e026278b997527452d7c371b0fa88311104e3517
18792720221 <2285633822@qq.com>
2021-01-19 06:22:06 +0800

    strcat

---
+++ strcat.cpp
@@ -0,0 +1,27 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//#include<string.h>
+//#include<stdlib.h>
+//#include<windows.h>
+//#include<assert.h>
+//char *my_strcat(char *str1, const char *str2)
+//{
+//	assert(str1&&str2);
+//	char*p = str1;
+//	while (*++str1);
+//	while (*str1++ = *str2++);
+//	return p;
+//}
+//
+//
+//
+//int main()
+//{
+//	char *p;
+//	char arr1[10] = "abc*";
+//	char arr2[] = "def";
+//	//p=strcat(arr1, arr2);
+//	p= my_strcat(arr1, arr2);
+//	printf("%s\n",arr1);
+//	return 0;
+//}
