4e2044235422b8402e84503fa6a7f6d4b2f2824b
18792720221 <2285633822@qq.com>
2021-01-28 19:41:57 +0800

    study

---
+++ day1-28.cpp
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<windows.h>
+
+int main()
+{
+	char str1[] = "hello bit.";
+	char str2[] = "hello bit.";
+	const char *str3= "hello bit.";
+	const char *str4= "hello bit.";
+	if (str1 == str2)
+		printf("str1 and str2 are same\n");
+	else
+		printf("str1 and str2 are not same\n");
+
+	if (str3 == str4)
+		printf("str3 and str4 are same\n");
+	else
+		printf("str3 and str4 are not same\n");
+
+	return 0;
+}
